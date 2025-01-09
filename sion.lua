9ff8127a4a77161e3576960639fc10630adac3ed
smallsaga2 <164740010+smallsaga2@users.noreply.github.com>
2025-01-09 18:19:19 +0900

    Update sion.lua
    tette

--- sion.lua
+++ sion.lua
@@ -1,35 +1,36 @@
 local gui = script.Parent.Parent.Parent
 local plr = game.Players.LocalPlayer
-local code_enter = plr.PlayerGui.ScreenGui.Frame.code_entering_box.Text
 local codes = {
-	plr.PlayerGui.ScreenGui.Codes.code1.Value,
-	plr.PlayerGui.ScreenGui.Codes.code2.Value,
-	plr.PlayerGui.ScreenGui.Codes.code3.Value,
+    plr.PlayerGui.ScreenGui.Codes.code1.Value,
+    plr.PlayerGui.ScreenGui.Codes.code2.Value,
+    plr.PlayerGui.ScreenGui.Codes.code3.Value,
 }
-local codjinwuiyeobu = true
 
 local message_correct = gui.codeCorrect
 local message_incorrect = gui.codeIncorrect
 
-script.parent.FocusLost:connect(function()
-	if code_enter == codes[1] then
-		codjinwuiyeobu = true
-	elseif code_enter ~= codes[1] then
-		codjinwuiyeobu = false
-	end
-end)
+-- FocusLost 이벤트
+script.Parent.FocusLost:Connect(function()
+    -- 입력값 가져오기
+    local code_enter = plr.PlayerGui.ScreenGui.Frame.code_entering_box.Text
+    local isCorrect = false
 
-script.parent.FocusLost:connect(function()
-	if codjinwuiyeobu == true then --코드 맞았을때
-		message_correct.Visible = true
-		wait(1)
-		message_correct.Visible = false
-		
-	elseif codjinwuiyeobu == false then -- 코드 틀렸을 때
-		message_incorrect.Visible = true
-		wait(1)
-		message_incorrect.Visible = false
-	end
-end)
+    -- 입력값과 코드 목록 비교
+    for _, code in ipairs(codes) do
+        if code_enter == code then
+            isCorrect = true
+            break
+        end
+    end
 
-print(codes[1])
+    -- 메시지 표시
+    if isCorrect then
+        message_correct.Visible = true
+        wait(1)
+        message_correct.Visible = false
+    else
+        message_incorrect.Visible = true
+        wait(1)
+        message_incorrect.Visible = false
+    end
+end)
